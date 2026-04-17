9ebdf579294b97f3688b7fecd2622d07729661ad
Icaro Albar <Icaro.eduardo@defensoria.rj.def.br>
2026-04-17 15:06:51 -0300

    debug: enable NextAuth debug mode temporarily

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import Cognito from "next-auth/providers/cognito"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
   providers: [
     Cognito({
       clientId: process.env.COGNITO_CLIENT_ID!,
